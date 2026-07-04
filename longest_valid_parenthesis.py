5a36705bdbeeaf9850e32c265fbd4a70a80233e1
maitysanskrita-art <maitysanskrita@gmail.com>
2026-07-04 12:56:47 +0530

    Create longest_valid_parenthesis.py

---
+++ longest_valid_parenthesis.py
@@ -0,0 +1,15 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack=[-1]
+        max_length=0
+        for i in range(len(s)):
+            if s[i]=='(':
+                stack.append(i)
+            else:
+                stack.pop()
+                if not stack:
+                    stack.append(i)
+                else:
+                    length=i-stack[-1]
+                    max_length=max(max_length,length)
+        return max_length
