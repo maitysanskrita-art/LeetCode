cdbd4ac6e28acb8b84bfe3845656f839f24e788a
maitysanskrita-art <maitysanskrita@gmail.com>
2026-07-22 22:53:15 +0530

    Create decode_ways.py

---
+++ decode_ways.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n=len(s)
+        dp=[0]*(n+1)
+        dp[0]=1
+        if s[0]!='0':
+            dp[1]=1
+        for i in range(2,n+1):
+            one_digit=s[i-1]
+            two_digits=s[i-2:i]
+            if one_digit!='0':
+                dp[i]+=dp[i-1]
+            if "10"<=two_digits<="26":
+                dp[i]+=dp[i-2]
+        return dp[n]
