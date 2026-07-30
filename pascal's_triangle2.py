3c87475d63d5d10d5fa86e84155181c1c9647c66
maitysanskrita-art <maitysanskrita@gmail.com>
2026-07-30 14:30:20 +0530

    Create pascal's_triangle2.py

---
+++ pascal's_triangle2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row=[1]
+        for i in range(1,rowIndex+1):
+            newRow=[1]*(i+1)
+            for j in range(1,i):
+                newRow[j]=row[j-1]+row[j]
+            row=newRow
+        return row       
