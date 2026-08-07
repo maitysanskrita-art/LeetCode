3116c404763e237b5969d49a3835c2b596e4673f
maitysanskrita-art <maitysanskrita@gmail.com>
2026-08-07 19:07:45 +0530

    Create max_points_on_line.py

---
+++ max_points_on_line.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        from collections import defaultdict
+        from math import gcd
+
+        n = len(points)
+        if n <= 2:
+            return n
+
+        ans = 1
+
+        for i in range(n):
+            slopes = defaultdict(int)
+
+            for j in range(i + 1, n):
+                dx = points[j][0] - points[i][0]
+                dy = points[j][1] - points[i][1]
+
+                if dx == 0:
+                    dy = 1
+                elif dy == 0:
+                    dx = 1
+                else:
+                    g = gcd(dx, dy)
+                    dx //= g
+                    dy //= g
+
+                    if dx < 0:
+                        dx *= -1
+                        dy *= -1
+
+                slopes[(dx, dy)] += 1
+                ans = max(ans, slopes[(dx, dy)] + 1)
+
+        return ans
