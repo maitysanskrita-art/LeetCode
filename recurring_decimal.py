1a938eca26f01adc2c32072211a6db72b9c62b82
maitysanskrita-art <maitysanskrita@gmail.com>
2026-08-10 11:23:32 +0530

    Create recurring_decimal.py

---
+++ recurring_decimal.py
@@ -0,0 +1,43 @@
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        
+
+        if numerator == 0:
+            return "0"
+
+        result = []
+
+        
+        if (numerator < 0) ^ (denominator < 0):
+            result.append("-")
+
+        numerator = abs(numerator)
+        denominator = abs(denominator)
+
+    
+        result.append(str(numerator // denominator))
+
+        remainder = numerator % denominator
+
+        if remainder == 0:
+            return "".join(result)
+
+        result.append(".")
+
+        seen = {}
+
+        while remainder:
+            if remainder in seen:
+                idx = seen[remainder]
+                result.insert(idx, "(")
+                result.append(")")
+                break
+
+            seen[remainder] = len(result)
+
+            remainder *= 10
+            result.append(str(remainder // denominator))
+            remainder %= denominator
+
+        return "".join(result)
+        
