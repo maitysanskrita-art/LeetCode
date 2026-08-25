8c04969efb63a2ac0b363e2b220bba217cd9cc7a
maitysanskrita-art <maitysanskrita@gmail.com>
2026-08-25 11:39:39 +0530

    Create course_schedule2.py

---
+++ course_schedule2.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+
+        for course, prerequisite in prerequisites:
+            graph[prerequisite].append(course)
+            indegree[course] += 1
+
+        queue = []
+
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+
+        result = []
+
+        while queue:
+            course = queue.pop(0)
+            result.append(course)
+
+            for next_course in graph[course]:
+                indegree[next_course] -= 1
+
+                if indegree[next_course] == 0:
+                    queue.append(next_course)
+
+        if len(result) == numCourses:
+            return result
+
+        return []
