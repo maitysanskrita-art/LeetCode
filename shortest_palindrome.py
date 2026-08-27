b864cbddc8518a680c32762846df96014419e01b
maitysanskrita-art <maitysanskrita@gmail.com>
2026-08-27 12:33:04 +0530

    Create shortest_palindrome.py

---
+++ shortest_palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        rev = s[::-1]
+        
+        temp = s + "#" + rev
+        
+        lps = [0] * len(temp)
+        
+        for i in range(1, len(temp)):
+            j = lps[i - 1]
+            
+            while j > 0 and temp[i] != temp[j]:
+                j = lps[j - 1]
+            
+            if temp[i] == temp[j]:
+                j += 1
+            
+            lps[i] = j
+        
+        longest = lps[-1]
+        
+        return rev[:len(s) - longest] + s
