4327244e61eab117ee72c2073ed9c547f0f74248
maitysanskrita-art <maitysanskrita@gmail.com>
2026-08-30 12:43:11 +0530

    Create basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,38 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        result = 0
+        number = 0
+        sign = 1
+
+        for ch in s:
+            if ch.isdigit():
+                number = number * 10 + int(ch)
+
+            elif ch == '+':
+                result += sign * number
+                number = 0
+                sign = 1
+
+            elif ch == '-':
+                result += sign * number
+                number = 0
+                sign = -1
+
+            elif ch == '(':
+                stack.append(result)
+                stack.append(sign)
+
+                result = 0
+                sign = 1
+
+            elif ch == ')':
+                result += sign * number
+                number = 0
+
+                sign = stack.pop()
+                result = stack.pop() + sign * result
+
+        result += sign * number
+
+        return result
